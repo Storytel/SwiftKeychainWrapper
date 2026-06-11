6c6d34a18a6ae7057d3bca935727c4b4b5d0f53d
anders <anders.pedersen@storytel.com>
2026-06-11 08:54:05 +0200

    Add platforms for Xcode 27

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftKeychainWrapper",
+    // Added for Xcode 27 compatibility (upstream archived)
+    platforms: [.iOS(.v12), .watchOS(.v5)],
     products: [
         .library(name: "SwiftKeychainWrapper", targets: ["SwiftKeychainWrapper"])
     ],
